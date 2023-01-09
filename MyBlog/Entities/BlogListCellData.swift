9d850c20989d68be731fe28d834154b1bd5cf097
bhoon-coding <bhooncoding@gmail.com>
2023-01-09 09:55:46 +0900

    feat: BlogListCell 구현

---
+++ MyBlog/Entities/BlogListCellData.swift
@@ -0,0 +1,15 @@
+//
+//  BlogListCellData.swift
+//  MyBlog
+//
+//  Created by BH on 2023/01/09.
+//
+
+import Foundation
+
+struct BlogListCellData {
+    let thumbnailURL: URL?
+    let name: String?
+    let title: String?
+    let dateTime: Data?
+}
---
+++ MyBlog/Presentation/BlogList.swift
@@ -0,0 +1,8 @@
+//
+//  BlogList.swift
+//  MyBlog
+//
+//  Created by BH on 2023/01/09.
+//
+
+import Foundation
