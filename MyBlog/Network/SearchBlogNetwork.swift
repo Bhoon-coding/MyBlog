5e773081d9e522a13c937ea773468bb46c922a06
bhoon-coding <bhooncoding@gmail.com>
2023-01-20 08:57:25 +0900

    chore: info.plist 수정 (API)

---
+++ MyBlog/Network/SearchBlogNetwork.swift
@@ -0,0 +1,15 @@
+//
+//  SearchBlogNetwork.swift
+//  MyBlog
+//
+//  Created by BH on 2023/01/11.
+//
+
+import Foundation
+
+class SearchBlogNetwork {
+    private let session: URLSession
+    let api = SearchBlogAPI()
+    
+    
+}
