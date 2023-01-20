4bdb5135e6248883f6c24e958d3cddbd904235af
bhoon-coding <bhooncoding@gmail.com>
2023-01-20 09:34:23 +0900

    feat: 네트워크 통신 (searchBlog 기능구현)

--- MyBlog/Network/SearchBlogNetwork.swift
+++ MyBlog/Network/SearchBlogNetwork.swift
@@ -5,11 +5,44 @@
 //  Created by BH on 2023/01/11.
 //
 
+import RxSwift
 import Foundation
+
+enum SearchNetworkError: Error {
+    case invalidJSON
+    case networkError
+}
 
 class SearchBlogNetwork {
     private let session: URLSession
     let api = SearchBlogAPI()
     
+    init(session: URLSession = .shared) {
+        self.session = session
+    }
     
+    func searchBlog(query: String) -> Single<Result<DKBlog, SearchNetworkError>> {
+        guard let url = api.searchBlog(query: query).url else {
+            return .just(.failure(.invalidJSON))
+        }
+        
+        let request = NSMutableURLRequest(url: url)
+        request.httpMethod = "GET"
+        request.setValue("KakaoAK faead46ff00591243483dc78523f0699", forHTTPHeaderField: "Authorization")
+        
+        return session.rx.data(request: request as URLRequest)
+            .map { data in
+                do {
+                    let blogData = try JSONDecoder().decode(DKBlog.self, from: data)
+                    return .success(blogData)
+                } catch {
+                    return .failure(.invalidJSON)
+                }
+            }
+            .catch { _ in
+                    .just(.failure(.networkError))
+            }
+            .asSingle()
+    }
+            
 }
